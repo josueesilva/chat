ce96724a728381d5914b55c43e4c0b2e65f25865
Josue <josueelias94@gmail.com>
2017-02-25 19:57:32 -0300

    modifing structure

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,7 +1,7 @@
 // Fill the DB with example data on startup
 
 import { Meteor } from 'meteor/meteor';
-import { Links } from '../../api/links/links.js';
+import { Links } from '../../api/links.js';
 
 Meteor.startup(() => {
   // if the Links collection is empty
