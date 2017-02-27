17cdaa86e5e5058b080bc56c74c5ab46f81001e2
Josue <josueelias94@gmail.com>
2017-02-27 12:42:35 -0300

    starting list users

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -3,10 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import { Conversations } from '../conversations.js';
 import { Mensages } from '../mensages.js';
 
-Meteor.publish('mensages.all', function (cId) {
+Meteor.publish('mensages', function (cId) {
   return Mensages.find({conversationId: cId});
 });
 
-Meteor.publish('users.all', function () {
+Meteor.publish('users', function () {
   return Meteor.users.find();
 });
--- imports/ui/components/contact-list/contact-list.js
+++ imports/ui/components/contact-list/contact-list.js
@@ -1,1 +1,15 @@
 import './contact-list.html';
+
+Template.contactList.onCreated(function () {
+  self = this;
+  self.autorun(function() {
+    self.subscribe("users");
+  });
+});
+
+
+Template.contactList.helpers({
+  users:() => {
+    return Meteor.users.find();
+  },
+});
