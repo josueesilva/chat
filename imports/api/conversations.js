c2809fd664752b9f45ad61b0ce3f8a8894c9c4af
Josue <josueelias94@gmail.com>
2017-02-27 01:13:55 -0300

    schemas

--- imports/api/conversations.js
+++ imports/api/conversations.js
@@ -1,4 +1,16 @@
-// Definition of the links collection
 
 import { Mongo } from 'meteor/mongo';
 Conversations = new Mongo.Collection('conversations');
+
+UserId = new SimpleSchema({
+  userId: {
+    type: String,
+  }
+});
+
+
+ConversationsSchema = new SimpleSchema({
+  usersIds: {
+    type: [UserId],
+  }
+});
--- imports/api/mensages.js
+++ imports/api/mensages.js
@@ -1,2 +1,29 @@
 import { Mongo } from 'meteor/mongo';
 Mensages = new Mongo.Collection('mensages');
+
+MensagesSchema = new SimpleSchema({
+  userName: {
+    type: String,
+    label: 'user name'
+  },
+  userId: {
+    type: String,
+    label:  'Id usuario'
+  },
+  conversationId:{
+    type:String,
+  },
+  Mensagem:{
+    type: String,
+    label: 'Mensagem'
+  },
+  dateSend:{
+    type: Date,
+    label: "Enviado as",
+    autoValue: function() {
+      return  new Date()
+    },
+  }
+});
+
+Mensages.attachSchema(MensagesSchema);
