8e2ecb94f16ace926d2fcbd0292b26d9c9adadd0
Josue <josueelias94@gmail.com>
2017-02-27 12:25:32 -0300

    add accounts

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -3,6 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import { Conversations } from '../conversations.js';
 import { Mensages } from '../mensages.js';
 
-Meteor.publish('Mensages.all', function () {
-  return Mensages.find();
+Meteor.publish('mensages.all', function (cId) {
+  return Mensages.find({conversationId: cId});
+});
+
+Meteor.publish('users.all', function () {
+  return Meteor.users.find();
 });
