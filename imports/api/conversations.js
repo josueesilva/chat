c2e5e71a02ce041a967d5a4bf3d7b831e5771344
Josue <josueelias94@gmail.com>
2017-02-27 01:04:26 -0300

    collections

---
+++ imports/api/conversations.js
@@ -0,0 +1,4 @@
+// Definition of the links collection
+
+import { Mongo } from 'meteor/mongo';
+Conversations = new Mongo.Collection('conversations');
---
+++ imports/api/mensages.js
@@ -0,0 +1,2 @@
+import { Mongo } from 'meteor/mongo';
+Mensages = new Mongo.Collection('mensages');
